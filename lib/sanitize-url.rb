652d2154f1e840e3a34fd7a84858bc2e31c570d2
jarrett <jarrett@uchicago.edu>
2010-02-25 14:11:01 -0600

    A bunch of fixes for numerical code references

--- lib/sanitize-url.rb
+++ lib/sanitize-url.rb
@@ -1,10 +1,43 @@
 module SanitizeUrl
+	# We only want to dereference certain numerical character references:
+	# numbers, letters, : and /
+	def self.dereference_code?(decimal_code)
+		(decimal_code >= 47 and decimal_code <= 58) or # Numbers
+		(decimal_code >= 65 and decimal_code <= 90) or # Uppercase letters
+		(decimal_code >= 97 and decimal_code <= 122)   # Lowercase letters
+	end
+	
+	# Decode numeric character references to numbers and letters.
+	# The purpose of this is to de-obfuscate malicious strings. Should be
+	# transparent to UTF-8, as multi-byte chars should be split up and then
+	# reassembled correctly in the output. The character codes for control
+	# characters like & and ; cannot appear as part of multi-byte characters,
+	# so there's no danger of interpreting part of a multi-byte char as
+	# a control character.
+	#
+	# This is a module method so that it won't pollute the namespace into
+	# which the module is mixed in.
+	def self.decode_numeric_references(str)
+		# Decimal code points, e.g. &#106; &#106 &#0000106; &#0000106
+		result = str.gsub(/&#([a-fA-f0-9]+);?/) do |match|
+			code = $1.to_i
+			dereference_code?(code) ? code.chr : match
+		end
+		# Hex code points, e.g. &#x6A; &#x6A
+		result.gsub(/&#[xX]([a-fA-f0-9]+);?/) do |match|
+			code = $1.to_i(16)
+			dereference_code?(code) ? code.chr : match
+		end
+	end
+	
 	def sanitize_url(url, options = {})
 		raise(ArgumentError, 'options[:protocols] must be an array') if options.has_key?(:protocols) and !options[:protocols].is_a?(Array)
 		options = {
 			:replace_evil_with => '',
 			:protocols => ['http', 'https', 'ftp', 'ftps', 'mailto', 'svn', 'svn+ssh', 'git']
 		}.merge(options)
+		# Decode numeric character references to deobsfuscate
+		url = SanitizeUrl.decode_numeric_references(url)
 		# Fix broken protocols
 		protocol = nil
 		url = url.sub(/^(([a-zA-Z\s]+)[\/:]+)/) do
--- spec/sanitize_url_spec.rb
+++ spec/sanitize_url_spec.rb
@@ -22,15 +22,15 @@ describe SanitizeUrl do
 		
 		it 'replaces data: URLs with options[:replace_evil_with]' do
 			urls = [
-				'data:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				'data://text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				'data//:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				'data/:/text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				' data:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				'da ta:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				'Data:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+',
-				"da\nta:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+",
-				"da\rta:text/html;base64,PHNjcmlwdD5hbGVydCgnZXZpbCcpPC9zY3JpcHQ+",
+				'data:text/html;base64,PHNjcmlwdD5hbGVydCgnMScpPC9zY3JpcHQ+',
+				'data://text/html;base64,PHNjcmlwdD5hbGVydCgnMicpPC9zY3JpcHQ+',
+				'data//:text/html;base64,PHNjcmlwdD5hbGVydCgnMycpPC9zY3JpcHQ+',
+				'data/:/text/html;base64,PHNjcmlwdD5hbGVydCgnNCcpPC9zY3JpcHQ+',
+				' data:text/html;base64,PHNjcmlwdD5hbGVydCgnNScpPC9zY3JpcHQ+',
+				'da ta:text/html;base64,PHNjcmlwdD5hbGVydCgnNicpPC9zY3JpcHQ+',
+				'Data:text/html;base64,PHNjcmlwdD5hbGVydCgnNycpPC9zY3JpcHQ+',
+				"da\nta:text/html;base64,PHNjcmlwdD5hbGVydCgnOCcpPC9zY3JpcHQ+",
+				"da\rta:text/html;base64,PHNjcmlwdD5hbGVydCgnOScpPC9zY3JpcHQ+",
 			].each do |evil_url|
 				sanitize_url(evil_url, :replace_evil_with => 'replaced').should == 'replaced'
 			end
@@ -66,6 +66,15 @@ describe SanitizeUrl do
 			sanitize_url('www.example.com').should == 'http://www.example.com'
 		end
 		
+		it 'replaces evil URLs that are encoded with numerical character references' do
+			[
+				'&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#49;&#39;&#41;',
+				'&#x6A;&#x61;&#x76;&#x61;&#x73;&#x63;&#x72;&#x69;&#x70;&#x74;&#x3A;&#x61;&#x6C;&#x65;&#x72;&#x74;&#x28;&#x27;&#x32;&#x27;&#x29;'
+			].each do |evil_url|
+				sanitize_url(evil_url, :replace_evil_with => 'replaced').should == 'replaced'
+			end
+		end
+		
 		it 'fixes common mistakes in the protocol' do
 			sanitize_url('http//www.example.com').should == 'http://www.example.com'
 			sanitize_url('http/www.example.com').should == 'http://www.example.com'
@@ -82,6 +91,80 @@ describe SanitizeUrl do
 			sanitize_url('http://www.example.com').should == 'http://www.example.com'
 			sanitize_url('https://www.example.com').should == 'https://www.example.com'
 			sanitize_url('ftp://www.example.com').should == 'ftp://www.example.com'
+		end
+	end
+	
+	describe '.decode_numeric_references' do
+		it 'decodes numbers' do
+			(0..9).each do |digit|
+				# Digit code points start at decimal 48
+				SanitizeUrl.decode_numeric_references('&#' + (48 + digit).to_s + ';').should == digit.to_s
+			end
+		end
+		
+		it 'decodes lowercase letters' do
+			# Lowercase code point range is 97-122
+			(97..122).each do |code_point|
+				SanitizeUrl.decode_numeric_references('&#' + code_point.to_s + ';').should == code_point.chr
+			end
+		end
+		
+		it 'decodes uppercase letters' do
+			# Uppercase code point range is 65-90
+			(65..90).each do |code_point|
+				SanitizeUrl.decode_numeric_references('&#' + code_point.to_s + ';').should == code_point.chr
+			end
+		end
+		
+		it 'decodes forward slashes' do
+			SanitizeUrl.decode_numeric_references('&#47').should == '/'
+		end
+		
+		it 'decodes colons' do
+			SanitizeUrl.decode_numeric_references('&#58').should == ':'
+		end
+		
+		it 'does not decode special characters other than forward slashes and colons' do
+			# Anything between 0 and 127 excluding 48-57, 65-90, and 97-122
+			((0..46).to_a + (59..64).to_a + (91..96).to_a + (123..127).to_a).each do |code_point|
+				reference = '&#' + code_point.to_s + ';'
+				SanitizeUrl.decode_numeric_references(reference).should == reference
+			end
+		end
+		
+		it 'does not decode references to characters outside the ASCII range' do
+			# Hebrew Alef as a Unicode code point, in hex and decimal
+			SanitizeUrl.decode_numeric_references('&#x05D0;').should == '&#x05D0;'
+			SanitizeUrl.decode_numeric_references('&#1488').should == '&#1488'
+		end
+		
+		it 'decodes short-form decimal UTF-8 character references with a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#106;').should == 'j'
+		end
+		
+		it 'decodes short-form decimal UTF-8 character references without a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#106').should == 'j'
+		end
+		
+		it 'decodes long-form decimal UTF-8 character references with a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#0000106;').should == 'j'
+		end
+		
+		it 'decodes long-form decimal UTF-8 character references without a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#0000106').should == 'j'
+		end
+		
+		it 'decodes hex UTF-8 character references with a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#x6A;').should == 'j'
+		end
+		
+		it 'decodes hex UTF-8 character references without a semicolon' do
+			SanitizeUrl.decode_numeric_references('&#x6A').should == 'j'
+		end
+		
+		it 'passes through multi-byte UTF-8 characters that are not URL-encoded' do
+			# Hebrew Alef in UTF-8
+			SanitizeUrl.decode_numeric_references("\xD7\x90").should == "\xD7\x90"
 		end
 	end
 end
